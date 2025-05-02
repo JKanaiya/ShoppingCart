3b6a439ecd560586bdb2145c97faf079f8f805b7
jonathan <jonathankanaiya@gmail.com>
2025-05-02 16:35:14 +0300

    minor change

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -2,6 +2,22 @@ import { describe, it, expect } from "vitest";
 import { screen, render } from "@testing-library/react";
 import Cart from "../components/Cart";
 
+const cartItems = [
+  {
+    id: 0,
+    title: "TestTitle",
+    description: "Jeans",
+    price: "Testing price",
+    image: "Testing image",
+  },
+  {
+    id: 1,
+    title: "TestTitle2",
+    description: "Coat",
+    price: "Testing price",
+    image: "Testing image",
+  },
+];
 describe("Cart", () => {
   it("Renders Cart is empty if it is empty", () => {
     render(<Cart cartItems={[]} />);
@@ -10,22 +26,6 @@ describe("Cart", () => {
     ).toBeInTheDocument();
   });
   it("Renders Cart Items when cartItems passed in", () => {
-    const cartItems = [
-      {
-        id: 0,
-        title: "TestTitle",
-        description: "Jeans",
-        price: "Testing price",
-        image: "Testing image",
-      },
-      {
-        id: 1,
-        title: "TestTitle2",
-        description: "Coat",
-        price: "Testing price",
-        image: "Testing image",
-      },
-    ];
     render(<Cart cartItems={cartItems} />);
     expect(screen.getByText(/testtitle2/i)).toBeInTheDocument();
   });
